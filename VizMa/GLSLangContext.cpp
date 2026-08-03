305911d205b11ebc89babeb134f7515beaacc53c
SaffronWare <aryanshadzi@gmail.com>
2026-08-03 10:27:08 -0400

    whoops, had forgotten to link the libs, now linked + compiles again

--- VizMa/GLSLangContext.cpp
+++ VizMa/GLSLangContext.cpp
@@ -1,6 +1,9 @@
 #include "GLSLangContext.h"
 
-GLSLangContext::GLSLangContext() { glslang::InitializeProcess(); }
+GLSLangContext::GLSLangContext() { 
+	glslang::InitializeProcess();
+	LOG("SUPPORTED GLSL VERSION " << glslang::GetGlslVersionString());
+}
 
 GLSLangContext::~GLSLangContext() { glslang::FinalizeProcess(); }
 

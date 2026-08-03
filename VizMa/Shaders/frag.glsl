2977072b9b178ffb3547fe84c68d67520d2f0378
SaffronWare <aryanshadzi@gmail.com>
2026-08-03 01:12:10 -0400

    added colors to shaders, tomorrow gotta figure out how to compile SPIRV, im extrmeely cloe to being able to actually do stuff now holy.

--- VizMa/Shaders/frag.glsl
+++ VizMa/Shaders/frag.glsl
@@ -1,11 +1,9 @@
 #version 450
 
-vec2 positions[3] = vec2[](
-    vec2(0.0, -0.5),
-    vec2(0.5, 0.5),
-    vec2(-0.5, 0.5)
-);
+layout(location = 0) out vec4 outColor;
+
+layout(location = 0) in vec3 fragColor;
 
 void main() {
-    gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
+    outColor = vec4(fragColor, 1.0);
 }

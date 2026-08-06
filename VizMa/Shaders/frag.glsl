9f96c9da976f5045b4f5548a6b16848587bf5735
SaffronWare <aryanshadzi@gmail.com>
2026-08-06 19:39:30 -0400

    added uv to frag.glsl ,starting ray marching

--- VizMa/Shaders/frag.glsl
+++ VizMa/Shaders/frag.glsl
@@ -3,7 +3,9 @@
 layout(location = 0) out vec4 outColor;
 
 layout(location = 0) in vec3 fragColor;
+layout(location = 1) in vec2 uv;
 
 void main() {
     outColor = vec4(fragColor, 1.0);
+    outColor = vec4(uv.x, uv.y, 0.0, 1.0);
 }
--- VizMa/Shaders/vert.glsl
+++ VizMa/Shaders/vert.glsl
@@ -1,9 +1,9 @@
 #version 450
 
 vec2 positions[3] = vec2[](
-    vec2(0.0, -0.5),
-    vec2(0.5, 0.5),
-    vec2(-0.5, 0.5)
+    vec2(0, 3),
+    vec2(-2, -1),
+    vec2(2, -1)
 );
 
 vec3 colors[3] = vec3[](
@@ -13,8 +13,10 @@ vec3 colors[3] = vec3[](
 );
 
 layout(location = 0) out vec3 fragColor;
+layout(location = 1) out vec2 uv;
 
 void main() {
     gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
     fragColor = colors[gl_VertexIndex];
+    uv = positions[gl_VertexIndex];
 }
